021f4b1f6f4252c001f6026f02b1700cc981f3e7
mrjclark <matthew-clark@live.com>
2026-05-30 11:04:51 -0500

    Add json utility file for json operations, update api utils to have all necessary api URLs

--- geocodingapi.md
+++
@@ -1,1 +0,0 @@
-https://nominatim.openstreetmap.org/search?q=17+Strada+Pictor+Alexandru+Romano%2C+Bukarest&format=geojson
---
+++ util/api-utils.sh
@@ -0,0 +1,3 @@
+geoapi=https://nominatim.openstreetmap.org/search?q=17+Strada+Pictor+Alexandru+Romano%2C+Bukarest&format=geojson
+histweatherapi=https://archive-api.open-meteo.com/v1/archive?latitude=$1&longitude=$2&start_date=$3&end_date=$4&hourly=$5&timezone=$6
+weatherapi=https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m
